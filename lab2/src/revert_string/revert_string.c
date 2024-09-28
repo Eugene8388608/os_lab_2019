73ad67642e71fc1db9cf753158119f3c8592132e
Eugene8388608 <15855754+Eugene8388608@users.noreply.github.com>
2024-09-28 07:54:15 +0000

    выполнена lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	for (char *l = str, *r = str + strlen(str) - 1; l < r; l++, r--)
+	{
+		*l ^= *r;
+		*r ^= *l;
+		*l ^= *r;
+	}
 }
 
